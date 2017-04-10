d536cacdd9dcd898dc89ea497d1c696a9c855224
Asim <asim.ibn.asghar@gmail.com>
2017-04-10 23:15:39 +0100

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const http = require('http');
+const express = require('express');
+const socketIO = require('socket.io');
+
+const publicPath = path.join(__dirname, '../public');
+const port = process.env.PORT || 3000;
+var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
+
+app.use(express.static(publicPath)); // html template path
+
+io.on('connection', (socket) => {
+    console.log('New user connected');
+
+    socket.on('createMessage', (message) => { // receiving
+        console.log('createMessage ', message);
+        io.emit('newMessage', { // io.emit send message to all the connections i.e. room main chat
+           from: message.from,
+           text: message.text,
+           createdAt: new Date().getTime()
+        });
+    });
+
+    socket.on('disconnect', () => {
+      console.log('User disconnected');
+    });
+});
+
+server.listen(port, () => {
+    console.log(`Server is up on port ${port}`);
+});
